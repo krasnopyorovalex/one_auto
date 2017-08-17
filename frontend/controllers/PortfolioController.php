9a647176ae8e90dd9a9a43d9ea0e211f581e772b
Aleksa2512 <djShtaket88@mail.ru>
2017-08-17 12:25:10 +0300

    disable validation csrf-token in PortfolioController beforeAction

--- frontend/controllers/PortfolioController.php
+++ frontend/controllers/PortfolioController.php
@@ -30,12 +30,20 @@ class PortfolioController extends SiteController
      */
     public function actionShow($id)
     {
-        $this->enableCsrfValidation = false;
         if(!$model = Portfolio::find()->where(['id' => $id])->with(['portfolioImages'])->one()){
             throw new NotFoundHttpException('The requested page does not exist.');
         }
         return $this->renderAjax('portfolio.twig',[
             'model' => $model
         ]);
+    }
+
+    /**
+     * @param \yii\base\Action $action
+     * @return bool
+     */
+    public function beforeAction($action) {
+        $this->enableCsrfValidation = false;
+        return parent::beforeAction($action);
     }
 }
